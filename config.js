311386f3d56d13d5e2cd27df3b32db11ab11a2f3
Virti X <hillaryoyieko6@gmail.com>
2024-12-13 07:45:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ global.author = "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋"
 global.channelname = "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋"
 global.channeljid = "120363315231436175@newsletter"
 global.autoTyping = false;                //make true to enable auto typing
-global.autoRecord = false;                //make true to enable auto recording
+global.autoRecord = true;                //make true to enable auto recording
 global.autoViewStatus = true;   // Set to true to automatically read statuses
 global.AUTOSTATUS_REACT = false;   // Set to true to automatically like statuses
 global.autoLikeEmoji = "💚";    // Default emoji for liking statuses
@@ -37,7 +37,7 @@ global.sudoUsers = ["2349066528353@s.whatsapp.net", "2348129988915@s.whatsapp.ne
 global.public = true
 
 
-global.ANTIDELETE = false;  
+global.ANTIDELETE = true;  
 
 global.unavailable = true;     
 //to show your real presence  
@@ -46,9 +46,9 @@ global.available = false;
 global.autoreadmessages = false;   
 ///To always read messages
 global.chatbot = false;
-global.autoreact = false;
+global.autoreact = true;
 
-global.WELCOME = false;
+global.WELCOME = true;
 
 global.prefix = '.';
 
@@ -56,11 +56,11 @@ global.autobio = false;
 
 global.ANTICALL = false;
 
-global.antilink = false;
+global.antilink = true;
 
 global.antilinkkick = false;
 
-global.antilinkwarn = false;
+global.antilinkwarn = true;
 
 global.mess = {
     success: '𝙳𝚘𝚗𝚎',
